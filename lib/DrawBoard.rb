bd64314ed14460b9873e4e36fc8f5b62352d1e8b
Aidas <Aidas>
2016-05-11 21:30:59 +0300

    Draws empty board

--- lib/DrawBoard.rb
+++ lib/DrawBoard.rb
@@ -1,3 +1,12 @@
 class DrawBoard
+  def self.draw_empty
+    board = Array.new(20)
+    (0...board.length).step(2) { |i| board[i] = Array.new(10, " |")}
+    (1...board.length).step(2) { |i| board[i] = Array.new(10, "--")}
+    puts "A B C D E F G H I J"
+    board.each { |x| puts x.join('')}
 
+  end
 end
+
+DrawBoard.draw_empty
